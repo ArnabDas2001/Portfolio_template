f542e64e2b062d6569cfb58e11a1746bca974996
unknown <dasarnab389@gmail.com>
2023-06-23 16:36:42 +0530

    adding darkmode

---
+++ script.js
@@ -0,0 +1,18 @@
+var typingEffect = new Typed(".multitext",{
+    strings : ["Seraphine", "a singer", "a mage"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500,
+})
+
+var moonIcon = document.getElementById("moonIcon");
+
+moonIcon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        moonIcon.src="sun.png";
+    }else{
+        moonIcon.src="moon.png";
+    }
+}
